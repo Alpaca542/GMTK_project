94b413c0943efc533d6585517f3c672b6499ada3
Krumss <yatkiu60@gmail.com>
2024-08-17 12:22:08 +0800

    Tornado bounds off edge

--- Assets/Scripts/Tornado.cs
+++ Assets/Scripts/Tornado.cs
@@ -8,14 +8,23 @@ public class Tornado : MonoBehaviour
 {
     Rigidbody2D rb;
     public float speed;
+    public Vector2 vel;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        rb.velocity = Random.insideUnitCircle * speed;
+        vel = rb.velocity;
     }
 
     private void Update()
     {
-        rb.velocity = transform.up * speed;
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        rb.velocity = Vector2.Reflect(vel, collision.GetContact(0).normal);
+        vel = rb.velocity;
     }
 }
