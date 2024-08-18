120df363473985b927745e5fa1846e639f17a7cf
Alpaca <mihaililyin08@gmail.com>
2024-08-18 10:43:47 +0400

    he walks :3

--- Assets/Scripts/Wizard.cs
+++ Assets/Scripts/Wizard.cs
@@ -13,6 +13,7 @@ public class wizard : MonoBehaviour
 
     [Header("Fields")]
     public Rigidbody2D rb;
+    public Animator anim;
     public GameObject wand;
 
     [Header("Debug")]
@@ -27,6 +28,48 @@ public class wizard : MonoBehaviour
     {
         float dirX = Input.GetAxis("Horizontal");
         float dirY = Input.GetAxis("Vertical");
+
+        float dirXRaw = Input.GetAxisRaw("Horizontal");
+        float dirYRaw = Input.GetAxisRaw("Vertical");
+
+        if (Mathf.Abs(dirXRaw) > 0 || Mathf.Abs(dirYRaw) > 0)
+        {
+            if (Mathf.Abs(dirYRaw) > 0)
+            {
+                if (dirYRaw > 0)
+                {
+                    anim.SetBool("GoRight", false);
+                    anim.SetBool("GoLeft", false);
+                    anim.SetBool("GoUp", true);
+                    anim.SetBool("GoDown", false);
+                }
+                else
+                {
+                    anim.SetBool("GoRight", false);
+                    anim.SetBool("GoLeft", false);
+                    anim.SetBool("GoUp", false);
+                    anim.SetBool("GoDown", true);
+                }
+            }
+            else
+            {
+                if (dirXRaw > 0)
+                {
+                    anim.SetBool("GoRight", true);
+                    anim.SetBool("GoLeft", false);
+                    anim.SetBool("GoUp", false);
+                    anim.SetBool("GoDown", false);
+                }
+                else
+                {
+                    anim.SetBool("GoRight", false);
+                    anim.SetBool("GoLeft", true);
+                    anim.SetBool("GoUp", false);
+                    anim.SetBool("GoDown", false);
+                }
+            }
+        }
+
         if (Input.GetMouseButton(0))
         {
             wand.SetActive(!wand.activeSelf);
