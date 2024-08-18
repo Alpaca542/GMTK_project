bcc2801c459edc8149af742bc44e86dd320f3cbd
Alpaca <mihaililyin08@gmail.com>
2024-08-18 20:30:29 +0400

    let's fucking goooo the water is great

--- Assets/Scripts/CarScripts/CarController.cs
+++ Assets/Scripts/CarScripts/CarController.cs
@@ -5,6 +5,8 @@ public class CarController : GoodThing
     [Header("Car Settings")]
     [SerializeField] private float speed = 7f;
 
+    private float currentVelocity;
+
     public Transform[] points;
     public bool isBadCar;
     private int currentPoint;
@@ -15,7 +17,7 @@ public class CarController : GoodThing
         direction.Normalize();
         // Move the car towards the end point
         transform.Translate(direction * speed * Time.deltaTime, Space.World);
-
+        LookAt(points[currentPoint].position);
         // Check if the car has reached the end point
         if (Vector2.Distance(transform.position, points[currentPoint].position) < 0.1f)
         {
@@ -25,5 +27,13 @@ public class CarController : GoodThing
                 Destroy(gameObject);
             }
         }
+    }
+
+    void LookAt(Vector3 what)
+    {
+        Vector2 movement = what - transform.position;
+        float targetAngle = Mathf.Atan2(-movement.x, movement.y) * Mathf.Rad2Deg + 90;
+        float smoothedAngle = Mathf.SmoothDampAngle(transform.eulerAngles.z, targetAngle, ref currentVelocity, 0.1f);
+        transform.rotation = Quaternion.Euler(0, 0, smoothedAngle);
     }
 }
